4dc760e5e2917fab228b1dc80d07e0d2a02f10a3
ricqCodes <princenwakanma1996@gmail.com>
2023-07-06 15:11:36 +0100

    Removed on unused variables

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { styled, css } from "styled-components";
+import { styled } from "styled-components";
 import { FaTwitter } from "react-icons/fa";
 
 import Logo from "../components/Logo";
